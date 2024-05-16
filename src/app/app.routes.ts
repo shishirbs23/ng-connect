4e1b42154d369de1719081e3fd42e91ceed1ac50
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-05-16 20:50:25 +0600

    set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
 
-export const routes: Routes = [];
+export enum RouteNames {
+  AUTH = 'auth',
+  PROFILE = 'profile',
+  USERS = 'users',
+  CHAT_LIST = 'chat-list',
+  CHAT_DETAILS = 'chat-details',
+}
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: RouteNames.AUTH,
+  },
+  {
+    path: RouteNames.AUTH,
+    component: AuthComponent,
+  },
+  {
+    path: RouteNames.AUTH,
+    loadComponent: () =>
+      import('./features/auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: RouteNames.PROFILE,
+    loadComponent: () =>
+      import('./features/profile/profile.component').then(
+        (m) => m.ProfileComponent
+      ),
+  },
+  {
+    path: RouteNames.USERS,
+    loadComponent: () =>
+      import('./features/users/users.component').then((m) => m.UsersComponent),
+  },
+  {
+    path: RouteNames.CHAT_LIST,
+    loadComponent: () =>
+      import('./features/chat-list/chat-list.component').then(
+        (m) => m.ChatListComponent
+      ),
+  },
+  {
+    path: RouteNames.CHAT_DETAILS,
+    loadComponent: () =>
+      import('./features/chat-details/chat-details.component').then(
+        (m) => m.ChatDetailsComponent
+      ),
+  },
+];
---
+++ src/app/features/chat-details/chat-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-details.component.html',
+  styleUrl: './chat-details.component.scss'
+})
+export class ChatDetailsComponent {
+
+}
