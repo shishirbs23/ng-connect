6467ebe9c60893f653aba4bac96e8f2b1202ae55
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-05-21 13:09:42 +0600

    add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCzH74o6YFFshx4DGtoYid5q1YtrdB4Vds',
+    authDomain: 'connect-me-92bc7.firebaseapp.com',
+    databaseURL: 'https://connect-me-92bc7-default-rtdb.firebaseio.com',
+    projectId: 'connect-me-92bc7',
+    storageBucket: 'connect-me-92bc7.appspot.com',
+    messagingSenderId: '185533995829',
+    appId: '1:185533995829:web:cc34413d8ed32e4e81ca7c',
+    measurementId: 'G-3T2YBKHJ59',
+  },
+};
