b83a4868210a78c601dab7f3d659b6468115e64b
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-05-29 13:23:25 +0600

    update app select component

--- src/app/core/components/app-select/app-select.component.ts
+++ src/app/core/components/app-select/app-select.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+// Services
+import { FormService } from '../../services/form.service';
+
+// Models
+import { FormField } from '../../../models/formField.model';
 
 @Component({
-  selector: 'app-app-select',
+  selector: 'app-select',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app-select.component.html',
   styleUrl: './app-select.component.scss'
 })
 export class AppSelectComponent {
-
+  field = model<FormField>({
+    id: 0,
+    isRequired: false,
+    label: '',
+    maxLength: 0,
+    minLength: 0,
+    name: '',
+    type: '',
+    value: '',
+    values: [],
+  });
+  formService = inject(FormService);
 }
