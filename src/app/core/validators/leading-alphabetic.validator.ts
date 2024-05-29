774c3bc5da63546a856d9f7858ec21420272260b
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-05-29 17:53:31 +0600

    add validator for leading alphabetic

---
+++ src/app/core/validators/leading-alphabetic.validator.ts
@@ -0,0 +1,22 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function LeadingAlphabeticValidator(
+  control: AbstractControl
+): ValidationErrors | null {
+  const value = control.value;
+
+  if (!value) {
+    return null;
+  }
+
+  const firstCharacter: string = value[0];
+
+  if (
+    (firstCharacter >= 'A' && firstCharacter >= 'Z') ||
+    (firstCharacter >= 'a' && firstCharacter >= 'z')
+  ) {
+    return null;
+  }
+
+  return { noLeadingAlphabetic: true };
+}
