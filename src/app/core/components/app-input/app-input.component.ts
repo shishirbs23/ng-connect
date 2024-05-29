19e6c80e6f3f2ba9c4485fbe9a88f89beebe485b
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-05-29 12:51:58 +0600

    create app input component

---
+++ src/app/core/components/app-input/app-input.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, input, model } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+// Models
+import { FormField } from '../../../models/formField.model';
+
+// Services
+import { FormService } from '../../services/form.service';
+
+// Pipes
+import { InputTypePipe } from '../../pipes/input-type.pipe';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  templateUrl: './app-input.component.html',
+  styleUrl: './app-input.component.scss',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    InputTypePipe,
+  ],
+})
+export class AppInputComponent {
+  field = model<FormField>({
+    id: 0,
+    isRequired: false,
+    label: '',
+    maxLength: 0,
+    minLength: 0,
+    name: '',
+    type: '',
+    value: '',
+    values: [],
+  });
+
+  /*  formService = input<FormService>(new FormService()); */
+
+  formService = inject(FormService);
+}
