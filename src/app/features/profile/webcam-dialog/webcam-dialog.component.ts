adc2c177b6fb026ce3f9a979e195eed8ec6eb697
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-05 15:42:52 +0600

    prepare webcame dialog component

--- src/app/features/profile/webcam-dialog/webcam-dialog.component.ts
+++ src/app/features/profile/webcam-dialog/webcam-dialog.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+// Models
+import { Profile } from '../../../models/profile.model';
+
+// Services
+import { ProfileService } from '../../../services/profile.service';
+import { WebcamService } from '../../../services/webcam.service';
 
 // Webcam
 import { WebcamModule } from 'ngx-webcam';
@@ -8,8 +19,25 @@ import { WebcamModule } from 'ngx-webcam';
 @Component({
   selector: 'app-webcam-dialog',
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, WebcamModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    WebcamModule,
+  ],
   templateUrl: './webcam-dialog.component.html',
   styleUrl: './webcam-dialog.component.scss',
 })
-export class WebcamDialogComponent {}
+export class WebcamDialogComponent {
+  profileService = inject(ProfileService);
+  service = inject(WebcamService);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { profile: Profile }) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.service.showWebCamOptions = true;
+    }, 1500);
+  }
+}
