b6cb6ef095f750deab7c860909cd54f5c6433a02
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-06 18:35:38 +0600

    add unique email validator

---
+++ src/app/core/validators/unique-email.validator.ts
@@ -0,0 +1,42 @@
+// Forms
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+
+// Firestore
+import { collection, where, query, getDocs } from 'firebase/firestore';
+
+// Services
+import { AppService } from '../services/app.service';
+
+// Enums
+import { ENTITY } from '../../utils/enums/entity.enum';
+import { Collection } from '../../utils/enums/collection.enum';
+import { Message } from '../../utils/constants/message';
+
+export class UniqueEmailValidator {
+  static createValidator(appService: AppService): AsyncValidatorFn {
+    return async (
+      control: AbstractControl
+    ): Promise<ValidationErrors | null> => {
+      const value = control.value;
+
+      if (!value) {
+        return null;
+      }
+
+      const profileQuery = query(
+        collection(appService._appDB, Collection.PROFILES),
+        where(ENTITY.EMAIL, '==', value)
+      );
+
+      const profileSnap = await getDocs(profileQuery);
+
+      return profileSnap.empty
+        ? null
+        : { uniqueEmail: Message.emailTaken };
+    };
+  }
+}
