1edfb3fda4f9bce41b4ea2d02ef5e6704ad62e20
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-13 19:29:46 +0600

    update UI and add necessary functionalities on header

--- src/app/core/components/app-header/app-header.component.ts
+++ src/app/core/components/app-header/app-header.component.ts
@@ -1,13 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 // Angular Material
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+// Components
+import { ImagePlaceholderComponent } from '../image-placeholder/image-placeholder.component';
 
 // Router
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+// Services
+import { AppService } from '../../services/app.service';
+import { AuthService } from '../../../services/auth.service';
+import { ProfileService } from '../../../services/profile.service';
 
 // Enums
 import { PageType } from '../../../utils/enums/page-type.enum';
@@ -15,7 +25,16 @@ import { PageType } from '../../../utils/enums/page-type.enum';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatBadgeModule, MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    RouterLink,
+    NgOptimizedImage,
+    ImagePlaceholderComponent,
+  ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss',
 })
@@ -24,4 +43,15 @@ export class AppHeaderComponent {
   centreTitle = input<boolean>(false);
   pageType = input<string>();
   pageTypes = PageType;
+
+  route = inject(ActivatedRoute);
+
+  appService = inject(AppService);
+  authService = inject(AuthService);
+  profileService = inject(ProfileService);
+
+  ngOnInit() {
+    const userId: string = this.appService.getRouteParamData(this.route, 'id');
+    userId && this.profileService.getProfileFromDb(userId);
+  }
 }
