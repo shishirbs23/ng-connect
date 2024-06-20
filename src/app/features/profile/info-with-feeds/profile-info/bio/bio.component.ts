82fcb2d578702f5ff3a8cf18cb5d67bc6e06b811
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-20 14:00:27 +0600

    create new component for bio

---
+++ src/app/features/profile/info-with-feeds/profile-info/bio/bio.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject } from '@angular/core';
+
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+// Forms
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+// Services
+import { ProfileService } from '../../../../../services/profile.service';
+
+@Component({
+  selector: 'profile-bio',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './bio.component.html',
+  styleUrl: './bio.component.scss',
+})
+export class BioComponent {
+  service = inject(ProfileService);
+  profileInfoForm!: FormGroup;
+
+  constructor() {
+    this.prepareForm();
+  }
+
+  prepareForm() {
+    this.profileInfoForm = new FormGroup({
+      bio: new FormControl(this.service.profile.bio, [
+        Validators.maxLength(200),
+      ]),
+    });
+  }
+
+  toggleBioEditor() {
+    this.service.isEditable.bio = !this.service.isEditable.bio;
+  }
+
+  async saveBio() {
+    this.profileInfoForm.disable();
+    this.service.profile.bio = this.profileInfoForm.value.bio;
+    await this.service.setProfile(this.service.profile, false, 'Bio Updated');
+    this.profileInfoForm.enable();
+    this.service.isEditable.bio = false;
+  }
+}
