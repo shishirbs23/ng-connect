4410a9bf4f3b6cec69bc79b308b22842b9db541e
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-25 20:59:07 +0600

    add initial UI for hobbies

--- src/app/features/profile/info-with-feeds/profile-info/hobbies/hobbies.component.ts
+++ src/app/features/profile/info-with-feeds/profile-info/hobbies/hobbies.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'profile-hobbies',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatIconModule,
+  ],
   templateUrl: './hobbies.component.html',
-  styleUrl: './hobbies.component.scss'
+  styleUrl: './hobbies.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HobbiesComponent {
+  readonly keywords = signal([
+    'angular',
+    'how-to',
+    'tutorial',
+    'accessibility',
+  ]);
+  readonly formControl = new FormControl(['angular']);
 
+  announcer = inject(LiveAnnouncer);
+
+  removeKeyword(keyword: string) {
+    this.keywords.update((keywords) => {
+      const index = keywords.indexOf(keyword);
+      if (index < 0) {
+        return keywords;
+      }
+
+      keywords.splice(index, 1);
+      this.announcer.announce(`removed ${keyword}`);
+      return [...keywords];
+    });
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our keyword
+    if (value) {
+      this.keywords.update((keywords) => [...keywords, value]);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
 }
