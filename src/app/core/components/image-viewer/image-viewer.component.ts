ae64059fe0be6e3f69f7bd27e01d5b5388974985
Shah Newaj Shishir <shah.newaz@brainstation-23.com>
2024-06-28 20:13:44 +0600

    improve image viewer UI

--- src/app/core/components/image-viewer/image-viewer.component.ts
+++ src/app/core/components/image-viewer/image-viewer.component.ts
@@ -1,4 +1,4 @@
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 
 // Angular Material
@@ -9,10 +9,41 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-image-viewer',
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, MatIconModule, NgOptimizedImage],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    NgOptimizedImage,
+  ],
   templateUrl: './image-viewer.component.html',
   styleUrl: './image-viewer.component.scss',
 })
 export class ImageViewerComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { url: string }) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { url: string }) {
+    this.adjustImageDimension();
+  }
+
+  adjustImageDimension() {
+    setTimeout(() => {
+      const container: HTMLElement | null = document.querySelector(
+        '.mat-mdc-dialog-content'
+      );
+
+      const image = new Image();
+      image.src = this.data.url;
+
+      image.onload = function () {
+        const { width, height } = image;
+
+        if (container) {
+          if (width > 750 || height > 750) {
+            container.style.backgroundSize = 'contain';
+          } else {
+            container.style.backgroundSize = `${width}px ${height}px`;
+          }
+        }
+      };
+    }, 0);
+  }
 }
